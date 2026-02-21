f7ae8f4ca4db292435a13cc9f0018da2980b7639
Ivan Revelino <230496953+vascocebola02-arch@users.noreply.github.com>
2026-02-21 22:59:13 +0200

    tentativa de implementacao fundo escuro

--- Projecto/scripts/script.js
+++ Projecto/scripts/script.js
@@ -1,4 +1,14 @@
 //alert("Este Web Site está em desenvolvimento, atualizaçōes futuras virão, mas enquanto isso aproveite-o :)");
+/*
+:root {
+     --dark: #444143;
+     --gray: #A19E9D;
+     --light: #E2E2DA;
+     --white: #FFFFFF;
+     --accent: #DCE8E7;
+}
+*/ 
+
 
 let logoIcon = document.getElementById("logo-icon");
 logoIcon.addEventListener("click", () => {
@@ -12,10 +22,27 @@ logoIcon.addEventListener("click", () => {
     }
 })
 
+function mudarCorDeFundo() {
+    //Sections e Header
+    let header = document.getElementById("header");
+    let mainHero = document.querySelector("#main-hero");
+    let menuNav = document.getElementById("#menuNav");
 
+    let botao1 = document.getElementById("button1")
+    let botao2 = document.getElementById("button2")
+    
+    botao1.style.backgroundColor = "#DCE8E7";
+    botao2.style.backgroundColor = "#DCE8E7";
 
+    header.style.backgroundColor = '#E2E2DA';
+    header.style.color = '#444143';
 
+    mainHero.style.backgroundColor = "#444143";
+    mainHero.style.color = "white";
+        
+    botao1.style.color = "#444143"
+    botao2.style.color = "#444143"
 
-//Parte da animacao
-
-let 
+    menuNav.style.backgroundColor = '#444143';
+    //Buttons
+}
